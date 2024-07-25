c6fd1bbd63bdca77a19e5e3680081a5d16eeb134
EddyVinck <eddyvinck95@gmail.com>
2024-07-26 00:24:36 +0200

    setup epic stack with neon postgres db

---
+++ app/utils/db.server.ts
@@ -0,0 +1,46 @@
+import { remember } from '@epic-web/remember'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+
+import chalk from 'chalk'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+export const prisma = remember('prisma', () => {
+	// NOTE: if you change anything in this function you'll need to restart
+	// the dev server to see your changes.
+
+	// Feel free to change this log threshold to something that makes sense for you
+	const logThreshold = 20
+
+	const pool = new Pool({ connectionString })
+	const adapter = new PrismaNeon(pool)
+	const client = new PrismaClient({
+		adapter,
+		log: [
+			{ level: 'query', emit: 'event' },
+			{ level: 'error', emit: 'stdout' },
+			{ level: 'warn', emit: 'stdout' },
+		],
+	})
+	client.$on('query', async (e) => {
+		if (e.duration < logThreshold) return
+		const color =
+			e.duration < logThreshold * 1.1
+				? 'green'
+				: e.duration < logThreshold * 1.2
+					? 'blue'
+					: e.duration < logThreshold * 1.3
+						? 'yellow'
+						: e.duration < logThreshold * 1.4
+							? 'redBright'
+							: 'red'
+		const dur = chalk[color](`${e.duration}ms`)
+		console.info(`prisma:query - ${dur} - ${e.query}`)
+	})
+	void client.$connect()
+	return client
+})
---
+++ tests/db-utils.ts
@@ -0,0 +1,144 @@
+import fs from 'node:fs'
+import { faker } from '@faker-js/faker'
+import { type PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import { UniqueEnforcer } from 'enforce-unique'
+
+const uniqueUsernameEnforcer = new UniqueEnforcer()
+
+export function createUser() {
+	const firstName = faker.person.firstName()
+	const lastName = faker.person.lastName()
+
+	const username = uniqueUsernameEnforcer
+		.enforce(() => {
+			return (
+				faker.string.alphanumeric({ length: 2 }) +
+				'_' +
+				faker.internet.userName({
+					firstName: firstName.toLowerCase(),
+					lastName: lastName.toLowerCase(),
+				})
+			)
+		})
+		.slice(0, 20)
+		.toLowerCase()
+		.replace(/[^a-z0-9_]/g, '_')
+	return {
+		username,
+		name: `${firstName} ${lastName}`,
+		email: `${username}@example.com`,
+	}
+}
+
+export function createPassword(password: string = faker.internet.password()) {
+	return {
+		hash: bcrypt.hashSync(password, 10),
+	}
+}
+
+let noteImages: Array<Awaited<ReturnType<typeof img>>> | undefined
+export async function getNoteImages() {
+	if (noteImages) return noteImages
+
+	noteImages = await Promise.all([
+		img({
+			altText: 'a nice country house',
+			filepath: './tests/fixtures/images/notes/0.png',
+		}),
+		img({
+			altText: 'a city scape',
+			filepath: './tests/fixtures/images/notes/1.png',
+		}),
+		img({
+			altText: 'a sunrise',
+			filepath: './tests/fixtures/images/notes/2.png',
+		}),
+		img({
+			altText: 'a group of friends',
+			filepath: './tests/fixtures/images/notes/3.png',
+		}),
+		img({
+			altText: 'friends being inclusive of someone who looks lonely',
+			filepath: './tests/fixtures/images/notes/4.png',
+		}),
+		img({
+			altText: 'an illustration of a hot air balloon',
+			filepath: './tests/fixtures/images/notes/5.png',
+		}),
+		img({
+			altText:
+				'an office full of laptops and other office equipment that look like it was abandoned in a rush out of the building in an emergency years ago.',
+			filepath: './tests/fixtures/images/notes/6.png',
+		}),
+		img({
+			altText: 'a rusty lock',
+			filepath: './tests/fixtures/images/notes/7.png',
+		}),
+		img({
+			altText: 'something very happy in nature',
+			filepath: './tests/fixtures/images/notes/8.png',
+		}),
+		img({
+			altText: `someone at the end of a cry session who's starting to feel a little better.`,
+			filepath: './tests/fixtures/images/notes/9.png',
+		}),
+	])
+
+	return noteImages
+}
+
+let userImages: Array<Awaited<ReturnType<typeof img>>> | undefined
+export async function getUserImages() {
+	if (userImages) return userImages
+
+	userImages = await Promise.all(
+		Array.from({ length: 10 }, (_, index) =>
+			img({ filepath: `./tests/fixtures/images/user/${index}.jpg` }),
+		),
+	)
+
+	return userImages
+}
+
+export async function img({
+	altText,
+	filepath,
+}: {
+	altText?: string
+	filepath: string
+}) {
+	return {
+		altText,
+		contentType: filepath.endsWith('.png') ? 'image/png' : 'image/jpeg',
+		blob: await fs.promises.readFile(filepath),
+	}
+}
+
+export async function cleanupDb(prisma: PrismaClient) {
+	const tables = await prisma.$queryRaw<
+		{ tablename: string }[]
+	>`SELECT tablename::text FROM pg_tables WHERE schemaname = 'public' AND tablename NOT LIKE '_prisma_migrations';`
+
+	try {
+		// Start a transaction
+		await prisma.$transaction(async (tx) => {
+			// Disable all triggers temporarily
+			await tx.$executeRaw`SET CONSTRAINTS ALL DEFERRED;`
+
+			// Truncate all tables in a single statement
+			const truncateSQL = tables
+				.map(({ tablename }) => `"${tablename}"`)
+				.join(', ')
+
+			if (truncateSQL) {
+				await tx.$executeRawUnsafe(`TRUNCATE TABLE ${truncateSQL} CASCADE;`)
+			}
+
+			// Re-enable triggers
+			await tx.$executeRaw`SET CONSTRAINTS ALL IMMEDIATE;`
+		})
+	} catch (error) {
+		console.error('Error cleaning up database:', error)
+	}
+}
